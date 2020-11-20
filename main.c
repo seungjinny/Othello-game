c5b71c06c02927f78cc4ab2004817931de9c9ec8
seungjinny <70585527+seungjinny@users.noreply.github.com>
2020-11-20 17:40:48 +0900

    2

--- main.c
+++ main.c
@@ -5,22 +5,37 @@
 
 void GameBoard () // making the gameboard of othello 
 {
-	int i,j;
 	
 	printf("   0 1 2 3 4 5 \n");
-	printf("  -------------\n");
+	printf("  -------------\n"); 
 	
-	for(i=0; i<6; i++)
-	{
-		printf("%d | | | | | | |\n", i);
-		printf("  -------------\n");
+	int board[6][6] = {
+	{0,0,0,0,0,0},
+	{0,0,0,0,0,0},
+	{0,0,1,2,0,0},
+	{0,0,2,1,0,0},
+	{0,0,0,0,0,0},
+	{0,0,0,0,0,0},
+	}; // 0: empty, 1: white, 2: black
+	
+	int i,r,c; // declaring the rows and columns of the board
+	
+	
 		
-	}
-		
+		for (r=0; r<6; r++)
+		{
+			for (c=0; c<6; c++)
+			
+			printf("%d|", board[r][c]);
+			printf("\n");
+			printf("-------------\n");
+   		}
+			
 }
 
 int main (void)
 {
+	printf("Welcome to the Othello Game!\n");
 	GameBoard();
 	
 	return 0;
