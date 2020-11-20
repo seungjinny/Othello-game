7e115795c525e5280a1b5defc1a310aad3d90a9b
seungjinny <70585527+seungjinny@users.noreply.github.com>
2020-11-20 19:30:32 +0900

    gameboard_of_othello

--- main.c
+++ main.c
@@ -1,15 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define size 6
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 void GameBoard () // making the gameboard of othello 
 {
 	
-	printf("   0 1 2 3 4 5 \n");
-	printf("  -------------\n"); 
+	printf("  0 1 2 3 4 5 \n");
+	printf(" -------------\n"); 
 	
-	int board[6][6] = {
+	int board[size][size] = {
 	{0,0,0,0,0,0},
 	{0,0,0,0,0,0},
 	{0,0,1,2,0,0},
@@ -18,25 +19,39 @@ void GameBoard () // making the gameboard of othello
 	{0,0,0,0,0,0},
 	}; // 0: empty, 1: white, 2: black
 	
-	int i,r,c; // declaring the rows and columns of the board
+	int r,c;
+	int name[size]={0,1,2,3,4,5};
+	 // declaring the rows and columns of the board
 	
 	
-		
-		for (r=0; r<6; r++)
+		for (r=0; r<size; r++)
 		{
-			for (c=0; c<6; c++)
+			printf("%d|", name[r]);
 			
-			printf("%d|", board[r][c]);
-			printf("\n");
-			printf("-------------\n");
+			{
+				for (c=0; c<size; c++)
+			
+				printf("%d|", board[r][c]);
+				printf("\n");
+				printf("-------------\n");
+			}
    		}
 			
+}
+
+void Status ()
+{
+	int a=2;
+	int b=2;
+	
+	printf("\n STATUS - WHITE: %d, BLACK: %d", a, b);
 }
 
 int main (void)
 {
 	printf("Welcome to the Othello Game!\n");
 	GameBoard();
+	Status();
 	
 	return 0;
 }
---
+++ 이름없음1.cpp
@@ -0,0 +1,11 @@
+void InvalidInput ()
+{
+	if (board[6][6] != 0)
+		printf("Invalid input: already occupied"); 
+	else if ()
+		printf("Invalid input: no flip happens");
+	else if ()
+		printf("Invalid input: input number should be less than 6");
+	else 
+		printf("");
+}
